b984bf89f41ab7751d052986d3810af32ee8c58b
Prateek kumar Upadhyay <Prateek.upadhyay@nab.com.au>
2020-03-11 09:47:17 +0000

    stg customer

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -5,7 +5,7 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from WARP_SAL_RT.SS1_SCHEMA1.RAW_CUSTOMERS
+    select * from {{ source('WARP_SAL_RT_SS1_SCHEMA1', 'stream_RAW_CUSTOMERS') }} --WARP_SAL_RT.SS1_SCHEMA1.RAW_CUSTOMERS
 
 ),
 
