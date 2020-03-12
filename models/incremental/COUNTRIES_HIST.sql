3ba4d548dc18d0ca50c5e6c825ac87454c66e1cb
Prateek kumar Upadhyay <Prateek.upadhyay@nab.com.au>
2020-03-12 05:41:17 +0000

    sd

--- models/incremental/COUNTRIES_HIST.sql
+++ models/incremental/COUNTRIES_HIST.sql
@@ -2,22 +2,21 @@
     config(
         materialized ='incremental',
         transient = 'false',
-        unique_key= "country_id"
-        unique_key= "header__timestamp"
+        unique_key = uqid 
     )
 }}
 
 WITH cte as (
   Select *,lead("header__timestamp") over (partition by country_id order by "header__change_seq","header__timestamp" ,"header__change_oper" ) as end_time_raw from "SNOWFL_RT01"."ADMIN"."COUNTRIES__ct"
-)
+),cte2 as 
 --MERGE INTO Countries_hist ch USING
-(Select c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
+(Select c.country_id+'_'+"header__timestamp" as uqid , c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
 from cte c 
 where 
   c.country_id in ( 'AE','SR')
  and c."header__operation" <> 'BEFOREIMAGE' 
  order by c.country_id, c."header__timestamp")
-
+Select * from cte2
 -- {% if is_incremental() %}
 
 --   -- this filter will only be applied on an incremental run
