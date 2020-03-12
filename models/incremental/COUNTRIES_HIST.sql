71cdd7e75a0ba22d463e5a3fbcf42fdc014f7ef0
Prateek kumar Upadhyay <Prateek.upadhyay@nab.com.au>
2020-03-12 02:10:17 +0000

    countries hist

--- models/incremental/COUNTRIES_HIST.sql
+++ models/incremental/COUNTRIES_HIST.sql
@@ -1,18 +1,24 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+        unique_key=['country_id ' ,"header__timestamp" ]
     )
 }}
 
-select
-    *,
-    my_slow_function(my_column)
-    
-from raw_app_data.events
+WITH cte as (
+  Select *,lead("header__timestamp") over (partition by country_id order by "header__change_seq","header__timestamp" ,"header__change_oper" ) as end_time_raw from "SNOWFL_RT01"."ADMIN"."COUNTRIES__ct"
+)
+--MERGE INTO Countries_hist ch USING
+(Select c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
+from cte c 
+where 
+  c.country_id in ( 'AE','SR')
+ and c."header__operation" <> 'BEFOREIMAGE' 
+ order by c.country_id, c."header__timestamp")
 
-{% if is_incremental() %}
+-- {% if is_incremental() %}
 
-  -- this filter will only be applied on an incremental run
-  where event_time > (select max(event_time) from {{ this }})
+--   -- this filter will only be applied on an incremental run
+--   where date_day >= (select max(date_day) from {{ this }})
 
-{% endif %}
+-- {% endif %}
