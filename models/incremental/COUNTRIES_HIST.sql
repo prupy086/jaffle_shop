2184038b8ea50b5c303649587dc8d7057e106044
Prateek kumar Upadhyay <Prateek.upadhyay@nab.com.au>
2020-03-12 05:47:23 +0000

    dfgdfg

--- models/incremental/COUNTRIES_HIST.sql
+++ models/incremental/COUNTRIES_HIST.sql
@@ -10,7 +10,7 @@ WITH cte as (
   Select *,lead("header__timestamp") over (partition by country_id order by "header__change_seq","header__timestamp" ,"header__change_oper" ) as end_time_raw from "SNOWFL_RT01"."ADMIN"."COUNTRIES__ct"
 ),cte2 as 
 --MERGE INTO Countries_hist ch USING
-(Select c.country_id+'_'+"header__timestamp" as uqid , c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
+(Select c.country_id ||'_'||CAST("header__timestamp" AS TEXT) as uqid  , c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
 from cte c 
 where 
   c.country_id in ( 'AE','SR')
