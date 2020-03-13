e4fbbd118f0c202cad50ad49c9da8f4070999d95
Prateek kumar Upadhyay <Prateek.upadhyay@nab.com.au>
2020-03-13 06:46:26 +0000

    czcz

--- models/incremental/COUNTRIES_HIST.sql
+++ models/incremental/COUNTRIES_HIST.sql
@@ -6,32 +6,6 @@
     )
 }}
 
-{{
-    config(
-        materialized ='incremental',
-        transient = 'false',
-        unique_key = 'uqid' 
-    )
-}}
-
-WITH cte as (
-  Select *,lead("header__timestamp") over (partition by country_id order by "header__change_seq","header__timestamp" ,"header__change_oper" ) as end_time_raw from "SNOWFL_RT01"."ADMIN"."COUNTRIES__ct"
-),final as 
---MERGE INTO Countries_hist ch USING
-(Select c.country_id ||'_'||CAST("header__timestamp" AS TEXT) as uqid  , c."header__operation",c.country_id,c.country_name, c.region_id,c.country_abr_name, c.Country_president_name, c."header__timestamp",c.end_time_raw
-from cte c 
-where 
-  c.country_id in ( 'AE','SR')
- and c."header__operation" <> 'BEFOREIMAGE' 
- {% if is_incremental() %}
-  -- this filter will only be applied on an incremental run
-  AND c."header__timestamp" >= (select max(c."header__timestamp") from {{ this }})
-{% endif %}
- order by c.country_id, c."header__timestamp")
-Select * from final
- 
-
- 
 with base as (
 Select country_id ||'|'||CAST("header__change_seq" AS TEXT) as uqid 
   ,*
